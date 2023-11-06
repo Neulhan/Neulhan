0ad75153a8b4a97b353089c2069121dd3d9e2b9f
neulhan <neulhan@icloud.com>
2023-11-06 18:21:20 +0900

    Feat : add sitemap feature

---
+++ web/src/routes/api/sitemap.xml/+server.ts
@@ -0,0 +1,35 @@
+import getPosts from '../posts/getPosts'
+
+export const prerender = true
+
+export async function GET() {
+    const posts = await getPosts()
+
+    return new Response(
+        `<?xml version="1.0" encoding="UTF-8" ?>
+        <urlset	xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"			
+                xmlns:xhtml="https://www.w3.org/1999/xhtml"			
+                xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"			
+                xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"			
+                xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"			
+                xmlns:video="https://www.google.com/schemas/sitemap-video/1.1">			
+                <url>
+                    <loc>http://blog.neulhan.com/</loc>
+                    <changefreq>daily</changefreq>
+                </url>	
+                <url>
+                    <loc>http://blog.neulhan.com/about</loc>
+                    <changefreq>daily</changefreq>
+                </url>
+                ${posts.map((post) => {
+                    return `<url>
+                        <loc>http://blog.neulhan.com/${post.slug}</loc>
+                        <lastmod>${post.date}</lastmod>
+                    </url>
+                    `
+                })}
+        </urlset>
+        `.trim(),
+        { headers: { 'Content-Type': 'application/xml' } }
+    )
+}
