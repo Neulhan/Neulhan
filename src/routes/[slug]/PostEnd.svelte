d5b511c3cff3896b01af76cd2eab4cebbbb43688
neulhan <neulhan@icloud.com>
2024-04-11 21:36:14 +0900

    Feat : delete js

--- src/routes/[slug]/PostEnd.svelte
+++ src/routes/[slug]/PostEnd.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
-import { popEffect } from '@src/lib/animations/popEffect.js'
-
-const back = (e: MouseEvent) => {
-    popEffect(e)
-    setTimeout(() => {
-        if (document.referrer && document.referrer.includes('mysite.com')) {
-            history.back()
-        } else {
-            location.href = '/'
-        }
-    }, 500)
-}
 </script>
 
 <div class="flex justify-between flex-col sm:flex-row sm:content-start">
-    <button on:click="{back}">
+    <a href="javascript:window.history.back();">
         <span class="tf">👈</span> 목록으로 돌아가기
-    </button>
-    <button on:click="{popEffect}" class="sm:mt-0 mt-2 relative">
+    </a>
+    <div class="sm:mt-0 mt-2 relative">
         <span class="tf pointer-events-none">😁</span> 부족한 글 읽어주셔서 감사합니다
-    </button>
+    </div>
 </div>
